03130938ea293a8db4f7a7f92fae706280fd621c
pzs <756306696@qq.com>
2019-06-18 17:04:04 +0800

    封装图表工具MPChartUtils.java

--- app/src/main/java/org/smile/mde/ui/activity/mpandroidchart/MPAndroidChartActivity.java
+++ app/src/main/java/org/smile/mde/ui/activity/mpandroidchart/MPAndroidChartActivity.java
@@ -1,8 +1,6 @@
 package org.smile.mde.ui.activity.mpandroidchart;
 
 import android.graphics.Color;
-import android.graphics.Typeface;
-import android.support.v4.content.ContextCompat;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
@@ -12,16 +10,10 @@ import android.util.Log;
 import android.widget.TextView;
 
 import com.example.commonlib.base.BaseActivity;
-import com.example.commonlib.mpandroidchart.PieValueFormatter;
-import com.example.commonlib.util.AppDebugUtil;
+import com.example.commonlib.mpandroidchart.view.FixPieChart;
 import com.example.commonlib.util.MPChartUtils;
 import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.Description;
-import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 
 import org.smile.mde.R;
@@ -36,7 +28,7 @@ public class MPAndroidChartActivity extends BaseActivity {
     @BindView(R.id.bar_chart)
     BarChart barChart;
     @BindView(R.id.pie_chart)
-    PieChart pieChart;
+    FixPieChart pieChart;
     @BindView(R.id.tv_name)
     TextView tv_name;
 
@@ -55,7 +47,7 @@ public class MPAndroidChartActivity extends BaseActivity {
 
     @Override
     protected void initData() {
-        setBarChartData();
+        showBarChart();
         showPieChart();
     }
 
@@ -68,66 +60,6 @@ public class MPAndroidChartActivity extends BaseActivity {
      * 饼状图：饼图的半径由控件宽高中更小的那个决定
      */
     private void showPieChart() {
-        pieChart.setLogEnabled(AppDebugUtil.isDebug());//打开图表的日志
-
-        pieChart.setDrawCenterText(false);  //饼状图中间文字不显示
-        Description description = pieChart.getDescription();
-        description.setText("我是Description()");//图形描述信息
-        description.setEnabled(false);//是否显示图形描述信息
-        pieChart.setRotationAngle(-90); // 设置饼图从哪个角度开始绘制
-        pieChart.setUsePercentValues(true);  //显示成百分比
-        //设置部分手势事件
-        pieChart.setTouchEnabled(false);// 设置是否可触摸，饼图要想响应手势事件，必须设为true
-        pieChart.setRotationEnabled(false); // 是否可以手动旋转
-        pieChart.setHighlightPerTapEnabled(false);//设置为false之后，点击每一块不能向外突出
-
-        //设置饼图中心孔显示的文本内容
-        pieChart.setDrawCenterText(true);  //饼状图中间可以添加文字
-        pieChart.setCenterText(getCenterText());
-        pieChart.setCenterTextColor(ContextCompat.getColor(getApplicationContext(), R.color.backgroup));
-        pieChart.setCenterTextSize(12f);
-
-        //设置显示饼图还是显示圆环
-        pieChart.setDrawHoleEnabled(true); //false:设置实心；   true:关闭实心，饼图由圆环和中心孔两部分组成
-        pieChart.setHoleRadius(56f);//当饼图以圆环显示时，设置中心孔的半径与饼图半径的百分比
-        //默认情况下，会绘制一个透明圆覆盖在饼图上，这里设置这个圆的半径与饼图半径的百分比，这个圆的背景色默认是半透明的
-        pieChart.setTransparentCircleRadius(1f);//如果不想显示这个透明圆，半径设置成一个很小的值（看源码，好像是小于setHoleRadius即可）
-        pieChart.setTransparentCircleColor(ContextCompat.getColor(getApplicationContext(), R.color.white));//设置透明圆的背景色
-        pieChart.setTransparentCircleAlpha(122);//设置透明圆背景色的透明度（取值范围0-255）
-
-        //设置饼图上面的文字，也就是PieEntry的label是否显示
-        pieChart.setDrawEntryLabels(false);
-        //设置饼图每一块的说明的字体颜色
-        pieChart.setEntryLabelColor(ContextCompat.getColor(getApplicationContext(), R.color.backgroup));
-        //设置饼图每一块说明的字体大小
-        pieChart.setEntryLabelTextSize(14f);
-        //设置字体，加粗Typeface.DEFAULT_BOLD
-        pieChart.setEntryLabelTypeface(Typeface.DEFAULT);
-
-        //图例示意图相关设置
-        Legend legend = pieChart.getLegend();
-        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);//设置竖直方向显示位置,legend.setYOffset设置偏移量
-        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);//设置水平方向显示位置，legend.setXOffset设置偏移量
-        legend.setForm(Legend.LegendForm.SQUARE);  //设置图例示意图形状，默认是方形
-        legend.setXEntrySpace(7f);//设置距离饼图的距离，防止与饼图重合
-        legend.setYEntrySpace(5f);
-        legend.setWordWrapEnabled(true);//设置允许示意图的内容换行，否则数据过多的时候会超出屏幕范围
-        legend.setDirection(Legend.LegendDirection.LEFT_TO_RIGHT);
-        legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);//内容排列方向
-        legend.setDrawInside(false);//可自行设置为true后查看效果
-        legend.setEnabled(false);//是否显示图例示意图
-
-        pieChart.setExtraOffsets(0, 8f, 0, 8f);//注意：查源码可知，此处8f指的是8dp，而不是8个像素
-        pieChart.setData(getPieData());
-        // undo all highlights
-        pieChart.highlightValues(null);
-        pieChart.invalidate();
-    }
-
-    /**
-     * 获取饼状图数据
-     */
-    private PieData getPieData() {
         List<Integer> colors = new ArrayList<>();
         colors.add(Color.rgb(205, 205, 205));
         colors.add(Color.rgb(114, 188, 223));
@@ -166,63 +98,33 @@ public class MPAndroidChartActivity extends BaseActivity {
         yValues.add(new PieEntry(0.2f, "在线2"));
         yValues.add(new PieEntry(0.3f, "离线2"));
 
-        // y轴的集合
-        PieDataSet pieDataSet = new PieDataSet(yValues, "");//第二个参数：示例图的描述信息，可以通过pieChart.getLegend()关掉
-        pieDataSet.setSliceSpace(0f); //设置每个饼状图之间的距离
-        // 饼图颜色
-        pieDataSet.setColors(colors);
-        pieDataSet.setSelectionShift(0f); //选中某部分时，这部分图形比其他图形突出的长度
-
-        //这一段代码就是实现加一个横线然后将模块的数据放在外面的效果
-        // 当值位置为外边线时，折线的前半段长度。
-        pieDataSet.setValueLinePart1Length(0.3f);
-        // 当值位置为外边线时，折线的后半段长度。
-        pieDataSet.setValueLinePart2Length(0.4f);
-        // 当ValuePosits为OutsiDice时，饼图显示：折线起始位置偏移占饼图半径大小的百分比；圆环显示：折线起始位置偏移占圆环半径大小的百分比；
-        pieDataSet.setValueLinePart1OffsetPercentage(70f);
-        //当值显示在界面外面的时候是否允许改变折线的长度
-        pieDataSet.setValueLineVariableLength(true);
-        // 当值位置为外边线时，设置线的宽度
-        pieDataSet.setValueLineWidth(1f);
-        // 当值位置为外边线时，表示线的颜色。
-        pieDataSet.setValueLineColor(ContextCompat.getColor(getApplicationContext(), R.color.backgroup));
-        //设置项X值拿出去
-        pieDataSet.setXValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
-        //设置将Y轴的值拿出去
-        pieDataSet.setYValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
-
-
-        PieData pieData = new PieData(pieDataSet);
-        // 是否在饼图上显示每个部分的百分比数值，也就是PieEntry中的value值
-        pieData.setDrawValues(true);
-        // 文字的大小
-        pieData.setValueTextSize(10);
-        // 文字的颜色
-        pieData.setValueTextColor(ContextCompat.getColor(getApplicationContext(), R.color.backgroup));
-        // 字体的样式，加粗DEFAULT_BOLD
-        pieData.setValueTypeface(Typeface.DEFAULT);
-        pieData.setValueFormatter(new PieValueFormatter(true));//格式化显示的数据为%百分比
-        return pieData;
+        MPChartUtils.initPieChart(pieChart, getCenterText(), R.color.backgroup);
+        pieChart.setData(MPChartUtils.getPieData(pieChart, colors, yValues));
+        // undo all highlights
+        pieChart.highlightValues(null);
+        pieChart.invalidate();
     }
 
     /**
      * 柱状图
      */
-    private void setBarChartData() {
+    private void showBarChart() {
+        //X轴方向
         List<String> xLabels = new ArrayList<>();
         xLabels.add("在线总数");
         xLabels.add("离线总数");
         xLabels.add("完好总数");
         xLabels.add("故障总数");
-        // 1.配置基础图表配置
-        MPChartUtils.configBarChart(barChart, xLabels);
-        // 2,初始化数据并绘制
-        ArrayList<BarEntry> yVals = new ArrayList<>();//Y轴方向第一组数组
-        yVals.add(new BarEntry(0, 1388));
-        yVals.add(new BarEntry(1, 612));
+        //Y轴方向
+        ArrayList<BarEntry> yVals = new ArrayList<>();
+        yVals.add(new BarEntry(0, 1988));
+        yVals.add(new BarEntry(1, 6));
         yVals.add(new BarEntry(2, 1797));
         yVals.add(new BarEntry(3, 203));
-        MPChartUtils.initBarChart(barChart, yVals, "镜头数", R.color.backgroup);
+
+        MPChartUtils.initBarChart(barChart, xLabels);
+        barChart.setData(MPChartUtils.getBarData(barChart, yVals, "镜头数", R.color.backgroup));
+        barChart.invalidate();
     }
 
     /**
--- commonlib/src/main/java/com/example/commonlib/util/MPChartUtils.java
+++ commonlib/src/main/java/com/example/commonlib/util/MPChartUtils.java
@@ -2,13 +2,17 @@ package com.example.commonlib.util;
 
 import android.graphics.Color;
 import android.graphics.Matrix;
+import android.graphics.Typeface;
 import android.support.v4.content.ContextCompat;
 
 import com.example.commonlib.R;
+import com.example.commonlib.mpandroidchart.PieValueFormatter;
+import com.example.commonlib.mpandroidchart.view.FixPieChart;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.Chart;
 import com.github.mikephil.charting.charts.CombinedChart;
 import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
@@ -19,6 +23,9 @@ import com.github.mikephil.charting.data.CombinedData;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.formatter.IValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
@@ -249,12 +256,12 @@ public class MPChartUtils {
     }
 
     /**
-     * 配置柱状图基础设置
+     * 初始化柱状图基础设置
      *
      * @param barChart
      * @param xLabels
      */
-    public static void configBarChart(BarChart barChart, final List<String> xLabels) {
+    public static void initBarChart(BarChart barChart, final List<String> xLabels) {
         barChart.getDescription().setEnabled(false);//设置不显示图形描述信息
         barChart.setPinchZoom(true);//设置按比例放缩柱状图
         barChart.setScaleEnabled(false);
@@ -326,14 +333,14 @@ public class MPChartUtils {
     }
 
     /**
-     * 初始化柱状图图表数据
+     * 获取柱状图图表数据
      *
      * @param chart
      * @param entries
      * @param title
      * @param barColor
      */
-    public static void initBarChart(BarChart chart, List<BarEntry> entries, String title, int barColor) {
+    public static BarData getBarData(BarChart chart, List<BarEntry> entries, String title, int barColor) {
         BarDataSet set = new BarDataSet(entries, title);
         //设置柱状的颜色
         set.setColor(ContextCompat.getColor(chart.getContext(), barColor));
@@ -356,8 +363,111 @@ public class MPChartUtils {
                 return String.valueOf(((int) (entry.getY())));//设置每个柱状头顶显示的文本内容
             }
         });
+        return data;
+    }
 
-        chart.setData(data);
-        chart.invalidate();
+    /**
+     * 初始化饼状图基础设置
+     * @param pieChart
+     * @param centerText
+     * @param centerTextColor
+     */
+    public static void initPieChart(FixPieChart pieChart, CharSequence centerText, int centerTextColor) {
+        pieChart.setLogEnabled(AppDebugUtil.isDebug());//打开图表的日志
+
+        pieChart.setDrawCenterText(false);  //饼状图中间文字不显示
+        Description description = pieChart.getDescription();
+        description.setText("我是Description()");//图形描述信息
+        description.setEnabled(false);//是否显示图形描述信息
+        pieChart.setRotationAngle(-90); // 设置饼图从哪个角度开始绘制
+        pieChart.setUsePercentValues(true);  //显示成百分比
+        //设置部分手势事件
+        pieChart.setTouchEnabled(false);// 设置是否可触摸，饼图要想响应手势事件，必须设为true
+        pieChart.setRotationEnabled(false); // 是否可以手动旋转
+        pieChart.setHighlightPerTapEnabled(false);//设置为false之后，点击每一块不能向外突出
+
+        //设置饼图中心孔显示的文本内容
+        pieChart.setDrawCenterText(true);  //饼状图中间可以添加文字
+        pieChart.setCenterText(centerText);
+        pieChart.setCenterTextColor(ContextCompat.getColor(pieChart.getContext(), centerTextColor));
+        pieChart.setCenterTextSize(12f);
+
+        //设置显示饼图还是显示圆环
+        pieChart.setDrawHoleEnabled(true); //false:设置实心；   true:关闭实心，饼图由圆环和中心孔两部分组成
+        pieChart.setHoleRadius(56f);//当饼图以圆环显示时，设置中心孔的半径与饼图半径的百分比
+        //默认情况下，会绘制一个透明圆覆盖在饼图上，这里设置这个圆的半径与饼图半径的百分比，这个圆的背景色默认是半透明的
+        pieChart.setTransparentCircleRadius(1f);//如果不想显示这个透明圆，半径设置成一个很小的值（看源码，好像是小于setHoleRadius即可）
+        pieChart.setTransparentCircleColor(ContextCompat.getColor(pieChart.getContext(), R.color.white));//设置透明圆的背景色
+        pieChart.setTransparentCircleAlpha(122);//设置透明圆背景色的透明度（取值范围0-255）
+
+        //设置饼图上面的文字，也就是PieEntry的label是否显示
+        pieChart.setDrawEntryLabels(false);
+        //设置饼图每一块的说明的字体颜色
+        pieChart.setEntryLabelColor(ContextCompat.getColor(pieChart.getContext(), R.color.backgroup));
+        //设置饼图每一块说明的字体大小
+        pieChart.setEntryLabelTextSize(14f);
+        //设置字体，加粗Typeface.DEFAULT_BOLD
+        pieChart.setEntryLabelTypeface(Typeface.DEFAULT);
+
+        //图例示意图相关设置
+        Legend legend = pieChart.getLegend();
+        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);//设置竖直方向显示位置,legend.setYOffset设置偏移量
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);//设置水平方向显示位置，legend.setXOffset设置偏移量
+        legend.setForm(Legend.LegendForm.SQUARE);  //设置图例示意图形状，默认是方形
+        legend.setXEntrySpace(7f);//设置距离饼图的距离，防止与饼图重合
+        legend.setYEntrySpace(5f);
+        legend.setWordWrapEnabled(true);//设置允许示意图的内容换行，否则数据过多的时候会超出屏幕范围
+        legend.setDirection(Legend.LegendDirection.LEFT_TO_RIGHT);
+        legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);//内容排列方向
+        legend.setDrawInside(false);//可自行设置为true后查看效果
+        legend.setEnabled(false);//是否显示图例示意图
+
+        pieChart.setExtraOffsets(0, 8f, 0, 8f);//注意：查源码可知，此处8f指的是8dp，而不是8个像素
+    }
+
+    /**
+     * 获取饼状图所需数据
+     * @param pieChart
+     * @param colors
+     * @param yValues
+     * @return
+     */
+    public static PieData getPieData(FixPieChart pieChart, List<Integer> colors, List<PieEntry> yValues) {
+        // y轴的集合
+        PieDataSet pieDataSet = new PieDataSet(yValues, "");//第二个参数：示例图的描述信息，可以通过pieChart.getLegend()关掉
+        pieDataSet.setSliceSpace(0f); //设置每个饼状图之间的距离
+        // 饼图颜色
+        pieDataSet.setColors(colors);
+        pieDataSet.setSelectionShift(0f); //选中某部分时，这部分图形比其他图形突出的长度
+
+        //这一段代码就是实现加一个横线然后将模块的数据放在外面的效果
+        // 当值位置为外边线时，折线的前半段长度。
+        pieDataSet.setValueLinePart1Length(0.3f);
+        // 当值位置为外边线时，折线的后半段长度。
+        pieDataSet.setValueLinePart2Length(0.4f);
+        // 当ValuePosits为OutsiDice时，饼图显示：折线起始位置偏移占饼图半径大小的百分比；圆环显示：折线起始位置偏移占圆环半径大小的百分比；
+        pieDataSet.setValueLinePart1OffsetPercentage(70f);
+        //当值显示在界面外面的时候是否允许改变折线的长度
+        pieDataSet.setValueLineVariableLength(true);
+        // 当值位置为外边线时，设置线的宽度
+        pieDataSet.setValueLineWidth(1f);
+        // 当值位置为外边线时，表示线的颜色。
+        pieDataSet.setValueLineColor(ContextCompat.getColor(pieChart.getContext(), R.color.backgroup));
+        //设置项X值拿出去
+        pieDataSet.setXValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
+        //设置将Y轴的值拿出去
+        pieDataSet.setYValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
+
+        PieData pieData = new PieData(pieDataSet);
+        // 是否在饼图上显示每个部分的百分比数值，也就是PieEntry中的value值
+        pieData.setDrawValues(true);
+        // 文字的大小
+        pieData.setValueTextSize(10);
+        // 文字的颜色
+        pieData.setValueTextColor(ContextCompat.getColor(pieChart.getContext(), R.color.backgroup));
+        // 字体的样式，加粗DEFAULT_BOLD
+        pieData.setValueTypeface(Typeface.DEFAULT);
+        pieData.setValueFormatter(new PieValueFormatter(true));//格式化显示的数据为%百分比
+        return pieData;
     }
 }
