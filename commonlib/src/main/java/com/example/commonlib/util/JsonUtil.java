8c22521f4922c91ba942fe76d3fd3d9409855a7a
pzs_smile <756306696@qq.com>
2019-08-30 16:13:36 +0800

    add BaiDuMapTrackActivity

--- commonlib/src/main/java/com/example/commonlib/util/JsonUtil.java
+++ commonlib/src/main/java/com/example/commonlib/util/JsonUtil.java
@@ -1,6 +1,12 @@
 package com.example.commonlib.util;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by smile on 2019/4/22.
@@ -21,5 +27,24 @@ public class JsonUtil {
             e.printStackTrace();
             return "";
         }
+    }
+
+    /**
+     * 转成list
+     * 解决泛型问题
+     *
+     * @param json
+     * @param cls
+     * @param <T>
+     * @return
+     */
+    public static  <T> List<T> jsonToList(String json, Class<T> cls) {
+        Gson gson = new Gson();
+        List<T> list = new ArrayList<T>();
+        JsonArray array = new JsonParser().parse(json).getAsJsonArray();
+        for (final JsonElement elem : array) {
+            list.add(gson.fromJson(elem, cls));
+        }
+        return list;
     }
 }
